abfd6c95682d9b474939080de969a2c70cb460be
danielFernandezJd <mdanif9728@gmail.com>
2024-03-15 13:47:32 -0700

    finish with the firs objects

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let newAsset = document.createElement("img")
+    newAsset.src = url
+    newAsset.style.position = "fixed"
+    newAsset.style.left = left + "px"
+    newAsset.style.bottom = bottom + "px"
+    document.body.append(newAsset)
+}
+// ••••••••••••••••• STATIC OBJECTS •••••••••••••••••
+// green-character
+newImage("assets/green-character.gif", 200, 200) 
+// pine-tree
+newImage("assets/pine-tree.png", 200, 300) 
+// pillar
+newImage("assets/pillar.png", 350, 100) 
+// crate
+newImage("assets/crate.png", 150, 200) 
+// well
+newImage("assets/well.png", 500, 425) 
+
+// ••••••••••••••••• COLLITION OBJECTS •••••••••••••••••
+
+
