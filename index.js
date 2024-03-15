8f87f52590170412381fe4ed7d0487c8f81529c2
danielFernandezJd <mdanif9728@gmail.com>
2024-03-15 15:25:34 -0700

    finish with all

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function newImage(url, left, bottom) {
+// ••••••••••••••••• STATIC OBJECTS ••••••••••••••••• \\
+function newImage(url, left, bottom, radius) {
     let newAsset = document.createElement("img")
     newAsset.src = url
     newAsset.style.position = "fixed"
@@ -6,7 +7,7 @@ function newImage(url, left, bottom) {
     newAsset.style.bottom = bottom + "px"
     document.body.append(newAsset)
 }
-// ••••••••••••••••• STATIC OBJECTS •••••••••••••••••
+
 // green-character
 newImage("assets/green-character.gif", 200, 200) 
 // pine-tree
@@ -18,6 +19,24 @@ newImage("assets/crate.png", 150, 200)
 // well
 newImage("assets/well.png", 500, 425) 
 
-// ••••••••••••••••• COLLITION OBJECTS •••••••••••••••••
 
+// ••••••••••••••••• GAMPLAY OBJECTS ••••••••••••••••• \\
+function clickImage (url, left, bottom){
+    let deletImage = document.createElement ("img")
+    deletImage.src = url 
+    deletImage.style.position = "fixed"
+    deletImage.style.left = left + "px"
+    deletImage.style.bottom = bottom + "px"
+    document.body.append(deletImage)
 
+    deletImage.addEventListener("click", function(){
+        deletImage.remove() 
+    })
+}
+
+// sword
+clickImage ("assets/sword.png", 500, 405)
+// shield
+clickImage ("assets/shield.png", 165, 185)
+// staff
+clickImage ("assets/staff.png", 600, 100)
